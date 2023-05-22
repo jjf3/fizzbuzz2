9b865208f8c8908f1bfcc6ab43e93ef23a4fc6bc
jjf3 <john.falco.slu@gmail.com>
2023-05-22 12:43:31 -0400

    Create FizzBuzz2.py
    Who is FizzBuzz?

---
+++ FizzBuzz2.py
@@ -0,0 +1,13 @@
+#Created on Mon May 22 12:39:13 2023
+#author: jjf3
+for fizzbuzz in range(51):
+    if fizzbuzz % 3 == 0 and fizzbuzz % 5 == 0:
+        print("fizzbuzz")
+        continue
+    elif fizzbuzz % 3 == 0:
+        print("fizz")
+        continue
+    elif fizzbuzz % 5 == 0:
+        print("buzz")
+        continue
+    print(fizzbuzz)
